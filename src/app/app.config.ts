555ed93cca40bef4654d7fb31315911b055b3c30
Carlos Serrano <csersan210@iesfidiana.es>
2024-11-05 10:33:04 +0100

    Api service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"recetario-a28a8","appId":"1:1098789838097:web:2e31772a79137e435d40b0","storageBucket":"recetario-a28a8.appspot.com","apiKey":"AIzaSyDikkSNhCmejNqPaNI17IyTurNd12YFm3o","authDomain":"recetario-a28a8.firebaseapp.com","messagingSenderId":"1098789838097","measurementId":"G-VG7EWL8ZNM"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes,withComponentInputBinding()), 
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp({"projectId":"recetario-a28a8","appId":"1:1098789838097:web:2e31772a79137e435d40b0","storageBucket":"recetario-a28a8.appspot.com","apiKey":"AIzaSyDikkSNhCmejNqPaNI17IyTurNd12YFm3o","authDomain":"recetario-a28a8.firebaseapp.com","messagingSenderId":"1098789838097","measurementId":"G-VG7EWL8ZNM"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({"projectId":"recetario-a28a8","appId":"1:1098789838097:web:2e31772a79137e435d40b0","storageBucket":"recetario-a28a8.firebasestorage.app","apiKey":"AIzaSyDikkSNhCmejNqPaNI17IyTurNd12YFm3o","authDomain":"recetario-a28a8.firebaseapp.com","messagingSenderId":"1098789838097","measurementId":"G-VG7EWL8ZNM"})), 
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ]
 };
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  api = inject( ApiService );
+
+  @Input() 
+  set type(type:string){
+    //1 - Analizar el valor,
+    //2 - Llamar al servicio
+    let resquest:Observable<any>;
+    switch(type){
+      case 'category':
+            resquest = this.api.getCategories();
+            break;
+      default:
+            resquest = this.api.getNationalities();
+    }
+
+    resquest.subscribe( (data:any) => console.log(data) );
+
+
+  }
+
+  ngOnInit(){
+
+
+  }
 
 }
