59e4c9acd0a5b02cd1d3b80e6cab68018ce58d6c
Carlos Serrano <csersan210@iesfidiana.es>
2024-11-05 11:16:54 +0100

    Home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, signal, WritableSignal } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,28 +11,59 @@ import { Observable } from 'rxjs';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  api = inject( ApiService );
+  api = inject(ApiService);
+  router = inject(Router);
 
-  @Input() 
-  set type(type:string){
+  $state:WritableSignal<any> = signal({
+    type: 'nationality',
+    loading: false,
+    error: false,
+    data: []
+  });
+
+
+
+  @Input()
+  set type(type: string) {
     //1 - Analizar el valor,
     //2 - Llamar al servicio
-    let resquest:Observable<any>;
-    switch(type){
+
+    //Hemos recibido un cambio en la ruta
+    this.$state.update(state => (
+      { ...state, loading: true, type: type }
+    )
+    );
+
+    let resquest: Observable<any>;
+    switch (type) {
       case 'category':
-            resquest = this.api.getCategories();
-            break;
+        resquest = this.api.getCategories();
+        break;
       default:
-            resquest = this.api.getNationalities();
+        resquest = this.api.getNationalities();
     }
 
-    resquest.subscribe( (data:any) => console.log(data) );
-
-
+    resquest.subscribe(
+      (data: any) => {
+        this.$state.update(state => (
+          { ...state, loading: false, error: false, 
+            data: data.map((m: any) => (
+              type == 'category' ? ({ name: m.strCategory }) : ({ name: m.strArea }))) 
+            }
+        ))
+        console.log(data)
+      },
+      (err) => {
+        console.log(err)
+        this.$state.update(state => (
+          { ...state, loading: false, error: err, data: [] }
+        ))
+      });
   }
 
-  ngOnInit(){
-
+  listRecipes(ingredient: string) {
+      //ir a la página /recipe/tipo/ingredient
+      this.router.navigate(['recipes', this.$state().type, ingredient]);
 
   }
 
